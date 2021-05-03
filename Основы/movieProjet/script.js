3a3253b34f736c24648102f555288ac8b64bf448
Alex <otostogan@yandex.ru>
2021-05-03 14:16:35 +0300

    MoovieProject Part1

--- Основы/movieProjet/script.js
+++ Основы/movieProjet/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
@@ -21,5 +22,25 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+      b = prompt('На сколько оцените его?', ""),
+      c = prompt('Один из последних просмотренных фильмов?', ""),
+      d = prompt('На сколько оцените его?', "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
