1fe44fce64923347cda5418455602e5e7d652d04
Alex <otostogan@yandex.ru>
2021-05-05 12:16:26 +0300

    Function

---
+++ Основы/Функции, стрелочные ф-ции(ES6)/script.js
@@ -0,0 +1,33 @@
+"use strict";
+let num = 20;
+function showFirstMessage(text){
+    console.log(text);
+    console.log(num);
+    // return num
+}
+
+
+showFirstMessage('Hello, world');
+console.log(num);
+
+
+
+function calc(a, b){
+    return(a + b);
+}
+
+console.log(calc(23, 33));
+console.log(calc(323, 33));
+console.log(calc(23, 33));
+
+function ret(){
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function(){
+    console.log('hello');
+};
