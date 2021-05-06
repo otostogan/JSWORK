76b28c934afe26506dc240a9950f88b561f55fc7
Alex <otostogan@yandex.ru>
2021-05-06 12:40:14 +0300

    MoovieProject part3

--- Основы/Методы и свойства строк и чисел/script.js
+++ Основы/Методы и свойства строк и чисел/script.js
@@ -1,9 +1,33 @@
 "use strict";
 
 
-const str = "test";
+const str = "tesT";
 const arr = [1,2,3];
 
-console.log(str.toUpperCase());
+// console.log(str.toUpperCase());
 console.log(str.toLowerCase());
 console.log(str);
+
+
+const fruit = "some fruit";
+
+console.log(fruit.indexOf("q"));
+
+
+const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+
+const num = 12.4;
+console.log(Math.round(num));
+
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
