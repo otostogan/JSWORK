43e3e65f6303b89bd9f389ea6eef1581887bd9bc
Alex <otostogan@yandex.ru>
2021-06-28 14:30:02 +0300

    .

---
+++ Продвинутый js/Event loop, подробная работа асинхронных и синхронных операций/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// let k = 0;
+
+// function count() {
+//     for(let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+//     alert('done');
+// }
+
+// count();
+
+
+setTimeout(()=>{
+    console.log(1);
+}, 0);
+
+console.log(2);
+
