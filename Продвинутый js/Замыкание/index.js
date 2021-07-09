3bf11aca55d62f036389eb9d0040b6382fa54dd9
Alex <otostogan@yandex.ru>
2021-07-09 13:55:07 +0300

    Замыкание

---
+++ Продвинутый js/Замыкание/index.js
@@ -0,0 +1,36 @@
+// function createCalcFunction(n) {
+//     return function(){
+//         console.log(1000*n);
+//     }
+// }
+// const calc = createCalcFunction(42)
+
+// calc()
+
+// function createIncrementor(n) {
+//     return function(num){
+//         return n + num
+//     }
+// }
+// const addOne = createIncrementor(1);
+// const addTen = createIncrementor(10);
+
+
+// console.log(addOne(10));
+// console.log(addOne(41));
+
+// console.log(addTen(10));
+// console.log(addTen(41));
+
+
+function urlGenerator(domain) {
+    return function(url){
+        return `https://${url}.${domain}`
+    }
+}
+
+const comUlr = urlGenerator('com')
+const ruUrl = urlGenerator('ru')
+
+console.log(comUlr('vk'));
+console.log(ruUrl('vk'));
