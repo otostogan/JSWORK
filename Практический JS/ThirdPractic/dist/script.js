ce953f5e3efa5fd5d6b298bce6b9a5f5080245a6
Alex <otostogan@yandex.ru>
2021-08-20 14:21:38 +0300

    ThirdPractic(Main_slider)

--- Практический JS/ThirdPractic/dist/script.js
+++ Практический JS/ThirdPractic/dist/script.js
@@ -974,6 +974,8 @@ function () {
   _createClass(Slider, [{
     key: "showSlides",
     value: function showSlides(n) {
+      var _this = this;
+
       if (n > this.slides.length) {
         this.slideIndex = 1;
       }
@@ -982,8 +984,24 @@ function () {
         this.slideIndex = this.slides.length;
       }
 
+      try {
+        this.hanson.style.opacity = '0';
+
+        if (n === 3) {
+          this.hanson.classList.add('animated');
+          setTimeout(function () {
+            _this.hanson.style.opacity = '1';
+
+            _this.hanson.classList.add('slideInUp');
+          }, 3000);
+        } else {
+          this.hanson.classList.remove('slideInUp');
+        }
+      } catch (e) {}
+
       this.slides.forEach(function (slide) {
         slide.style.display = 'none';
+        slide.classList.add('animated');
       });
       this.slides[this.slideIndex - 1].style.display = 'block';
     }
@@ -995,17 +1013,25 @@ function () {
   }, {
     key: "render",
     value: function render() {
-      var _this = this;
+      var _this2 = this;
+
+      try {
+        this.hanson = document.querySelector('.hanson');
+      } catch (e) {}
 
       this.btns.forEach(function (btn) {
         btn.addEventListener('click', function () {
-          _this.plusSlides(1);
+          _this2.plusSlides(1);
+
+          if (btn.classList.contains('next')) {
+            _this2.slides[_this2.slideIndex - 1].classList.add('fadeInLeftBig');
+          }
         });
         btn.parentNode.previousElementSibling.addEventListener('click', function (e) {
           e.preventDefault();
-          _this.slideIndex = 1;
+          _this2.slideIndex = 1;
 
-          _this.showSlides(_this.slideIndex);
+          _this2.showSlides(_this2.slideIndex);
         });
       });
       this.showSlides(this.slideIndex);
--- Практический JS/ThirdPractic/src/js/modules/slider.js
+++ Практический JS/ThirdPractic/src/js/modules/slider.js
@@ -13,20 +13,45 @@ export default class Slider {
         if(n < 1){
             this.slideIndex = this.slides.length;
         }
+
+        try{
+            this.hanson.style.opacity = '0';   
+
+            if(n === 3){
+                this.hanson.classList.add('animated');
+                setTimeout(()=>{
+                    this.hanson.style.opacity = '1';   
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+            
+        } catch(e){}
+
         this.slides.forEach(slide=>{
             slide.style.display = 'none';
+            slide.classList.add('animated');
         });
         this.slides[this.slideIndex-1].style.display = 'block';
     }
 
     plusSlides(n){
-        this.showSlides(this.slideIndex += n);
+        this.showSlides(this.slideIndex += n);   
     }
 
     render(){
+        try{
+            this.hanson = document.querySelector('.hanson');
+
+        } catch(e){}
+
         this.btns.forEach(btn => {
             btn.addEventListener('click', ()=>{
                 this.plusSlides(1);
+                if(btn.classList.contains('next')){
+                    this.slides[this.slideIndex - 1].classList.add('fadeInLeftBig');
+                }
             });
             btn.parentNode.previousElementSibling.addEventListener('click', (e)=>{
                 e.preventDefault();
