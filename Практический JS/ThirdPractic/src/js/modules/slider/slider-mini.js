ef72e899d7e7960d200a625e0e8ce020b9caf978
Alex <otostogan@yandex.ru>
2021-08-21 16:01:52 +0300

    ThirdPractic(DinamicVideoPlayer)

--- Практический JS/ThirdPractic/src/js/modules/slider/slider-mini.js
+++ Практический JS/ThirdPractic/src/js/modules/slider/slider-mini.js
@@ -40,20 +40,22 @@ export default class MiniSlider extends Slider {
     }
 
     bindTriggers(){
-        this.next.addEventListener('click', ()=> this.nextSlide());
-        this.prev.addEventListener('click', ()=>{
-
-            for(let i = this.slides.length - 1; i>0; i--){
-                if(this.slides[i].tagName !== "BUTTON"){
-                    let active = this.slides[i];
-                    this.container.insertBefore(active, this.slides[0]);
-                    this.decorizeSlides();
-                    break;
-                }
-            }
-
-            
+        this.next.forEach(btn=>{
+            btn.addEventListener('click', ()=> this.nextSlide());
         });
+        this.prev.forEach(btn=>{
+            btn.addEventListener('click', ()=> {
+                for(let i = this.slides.length - 1; i>0; i--){
+                    if(this.slides[i].tagName !== "BUTTON"){
+                        let active = this.slides[i];
+                        this.container.insertBefore(active, this.slides[0]);
+                        this.decorizeSlides();
+                        break;
+                    }
+                }
+            });
+        });
+        
     }
     activateAnimation(){
         if(this.autoplay){
@@ -78,12 +80,12 @@ export default class MiniSlider extends Slider {
 
 
             
-            this.container.addEventListener('mouseenter', ()=>{
-                clearInterval(this.paused);
-            });
-            this.container.addEventListener('mouseleave', ()=>{
-                this.activateAnimation();
-            });
+            // this.container.addEventListener('mouseenter', ()=>{
+            //     clearInterval(this.paused);
+            // });
+            // this.container.addEventListener('mouseleave', ()=>{
+            //     this.activateAnimation();
+            // });
         } catch(e){}
     }
 }
