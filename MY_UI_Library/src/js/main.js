2223af73065fa1d4056a19a4144d72facbeeb852
Alex <otostogan@yandex.ru>
2021-08-26 20:20:30 +0300

    My_UI_LIBRARY(FadeToggle)

--- MY_UI_Library/src/js/main.js
+++ MY_UI_Library/src/js/main.js
@@ -1,13 +1,15 @@
 import $ from './lib/lib';
 
 $('#first').on('click', () => {
-    $('.wrapper div').eq(0).fadeOut(800);
+    $('.wrapper div').eq(0).fadeToggle(800);
 });
 
 $('[data-count="second"]').on('click', () => {
-    $('.wrapper div').eq(1).fadeOut(800);
+    $('.wrapper div').eq(1).fadeToggle(800);
 });
 
 $('button').eq(2).on('click', () => {
-    $('.w-500px').fadeOut(800);
+    $('.w-500px').fadeToggle(800);
 }); 
+
+// $('#first').fadeIn(800, 'inline-block');
