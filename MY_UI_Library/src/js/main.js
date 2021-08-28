56e03c22987d58c7aaccde131bfac8e78af74c9d
Alex <otostogan@yandex.ru>
2021-08-28 09:44:03 +0300

    My_UI_LIBRARY(DropDownMenu)

--- MY_UI_Library/src/js/main.js
+++ MY_UI_Library/src/js/main.js
@@ -5,11 +5,26 @@ $('#first').on('click', () => {
 });
 
 $('[data-count="second"]').on('click', () => {
-    $('.wrapper div').eq(1).fadeToggle(800);
+    $('.wrapper div').eq(1).fadeToggle(800, 'inline-block');
 });
 
 $('button').eq(2).on('click', () => {
     $('.w-500px').fadeToggle(800);
 }); 
 
+$('.wrapp').html(
+    `
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">
+            Dropdown button
+        </button>
+        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+            <a href="#" class="dropdown-item">Action</a>
+            <a href="#" class="dropdown-item">Action #2</a>
+            <a href="#" class="dropdown-item">Action #3</a>
+        </div>
+    </div>            
+    `
+);
+$('.dropdown-toggle').dropdown();
 // $('#first').fadeIn(800, 'inline-block');
